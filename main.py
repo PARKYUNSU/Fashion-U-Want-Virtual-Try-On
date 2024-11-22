14a38c3a99a42c85ac5cdc0a0adcea0304956ee4
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2024-11-22 11:07:08 +0900

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
-from openpose import load_model, inference_and_save
-from clothseg import process_images
 import warnings
 import cv2
 import glob
 import argparse
+from openpose import load_model, inference_and_save
+from clothseg import process_images
 
 warnings.filterwarnings("ignore", category=FutureWarning)
 
@@ -37,6 +37,46 @@ def process_densepose(input_dir, output_dir):
         print(f"Saved DensePose result to {output_image_path}")
 
 
+def process_graphonomy(input_path, output_path):
+    """
+    Graphonomy-Master를 사용한 세분화 결과 생성
+    """
+    print("Generate semantic segmentation using Graphonomy-Master library\n")
+    os.chdir("./Graphonomy-master")
+    terminal_command = (
+        f"python exp/inference/inference.py --loadmodel ./inference.pth "
+        f"--img_path {input_path} --output_path {output_path} --output_name resized_segmentation_img"
+    )
+    os.system(terminal_command)
+    os.chdir("../")
+
+
+def run_hr_viton(background_flag):
+    """
+    HR-VITON 실행 및 배경 추가/제거 처리
+    """
+    print("\nRun HR-VITON to generate final image\n")
+    os.chdir("./HR-VITON-main")
+    terminal_command = (
+        "python3 test_generator.py --cuda True --test_name test1 --tocg_checkpoint mtviton.pth "
+        "--gpu_ids 0 --gen_checkpoint gen.pth --datasetting unpaired --data_list t2.txt --dataroot ./test"
+    )
+    os.system(terminal_command)
+    os.chdir("../")
+
+    print("\nProcessing HR-VITON output images")
+    output_images = glob.glob("./HR-VITON-main/Output/*.png")
+    mask_img = cv2.imread('./output/image-densepose/mask.png', cv2.IMREAD_GRAYSCALE)
+    background = cv2.imread('./input/background.jpg')
+
+    for image_path in output_images:
+        img = cv2.imread(image_path)
+        if background_flag and mask_img is not None:
+            img = cv2.bitwise_and(img, img, mask=mask_img)
+            img = cv2.add(img, background)
+        cv2.imwrite(image_path, img)
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--background', action='store_true', help='Add background to the final image')
@@ -53,7 +93,7 @@ def main():
     output_clothseg_path = './output/cloth_segment'
     output_parse_path = './output/human_parse'
 
-    # 출력 디렉토리
+    # 출력 디렉토리 생성
     os.makedirs(output_openpose_image_path, exist_ok=True)
     os.makedirs(output_openpose_json_path, exist_ok=True)
     os.makedirs(output_densepose_path, exist_ok=True)
@@ -71,51 +111,22 @@ def main():
         print(f'\nProcessing OpenPose for: {image_path}')
         inference_and_save(image_path, body_estimation, hand_estimation, output_openpose_image_path, output_openpose_json_path)
 
-        # DensePose 처리
-        print(f'\nProcessing DensePose for: {image_path}')
-        process_densepose(input_path, output_densepose_path)
+    # DensePose 처리
+    print(f'\nProcessing DensePose for images in: {input_path}')
+    process_densepose(input_path, output_densepose_path)
 
     # Cloth Segmentation 처리
     print(f'\nProcessing Cloth Segmentation for images in: input/cloth')
-    process_images(output_path=output_clothseg_path)  # Cloth segmentation 처리
+    process_images(output_path=output_clothseg_path)
 
-    # Graphonomy-Master를 사용한 세분화 결과 생성
-    print("Generate semantic segmentation using Graphonomy-Master library\n")
-    os.chdir("./Graphonomy-master")
-    terminal_command = f"python exp/inference/inference.py --loadmodel ./inference.pth --img_path {input_path} --output_path {output_parse_path} --output_name resized_segmentation_img"
-    os.system(terminal_command)
-    os.chdir("../")
-    
-    # Grayscale 세그멘테이션 처리
-    print("\nGenerate grayscale semantic segmentation image")
-    terminal_command = "python grayscale.py"
-    os.system(terminal_command)
+    # Graphonomy-Master 처리
+    process_graphonomy(input_path, output_parse_path)
 
-    # Run HR-VITON to generate final image
-    print("\nRun HR-VITON to generate final image\n")
-    os.chdir("./HR-VITON-main")
-    terminal_command = "python3 test_generator.py --cuda True --test_name test1 --tocg_checkpoint mtviton.pth --gpu_ids 0 --gen_checkpoint gen.pth --datasetting unpaired --data_list t2.txt --dataroot ./test"
-    os.system(terminal_command)
-    os.chdir("../")
-
-    # Add or remove background in HR-VITON output
-    print("\nProcessing HR-VITON output images")
-    l = glob.glob("./HR-VITON-main/Output/*.png")
-    mask_img = cv2.imread('./output/image-densepose/mask.png', cv2.IMREAD_GRAYSCALE)
-    back_ground = cv2.imread('./input/background.jpg')
-
-    if opt.background:
-        for i in l:
-            img = cv2.imread(i)
-            img = cv2.bitwise_and(img, img, mask=mask_img)
-            img = img + back_ground
-            cv2.imwrite(i, img)
-    else:
-        for i in l:
-            img = cv2.imread(i)
-            cv2.imwrite(i, img)
+    # HR-VITON 실행
+    run_hr_viton(opt.background)
 
     print("All processing is complete.")
+
 
 if __name__ == "__main__":
     main()
